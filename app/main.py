954ec53cc4d154e751ead69be8c4c63694517cb1
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-05-22 22:20:32 +0200

    feat: implement cache decorator with internal storage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        cache_key = (args, tuple(sorted(kwargs.items())))
+
+        if cache_key in storage:
+            print("Getting from cache")
+            return storage[cache_key]
+
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        storage[cache_key] = result
+
+        return result
+
+    return wrapper
